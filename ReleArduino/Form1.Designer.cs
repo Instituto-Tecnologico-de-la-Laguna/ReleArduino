237035c967d0c0161a0d8f3b5e9ca25ba5f6cd39
jmot4 <jmota@gtech-services.com>
2024-09-12 14:29:37 -0600

    Agregar archivos de proyecto.

---
+++ ReleArduino/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ReleArduino
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnRelevador = new Button();
+            SuspendLayout();
+            // 
+            // btnRelevador
+            // 
+            btnRelevador.BackColor = Color.IndianRed;
+            btnRelevador.Font = new Font("Segoe UI", 18F);
+            btnRelevador.Location = new Point(114, 39);
+            btnRelevador.Name = "btnRelevador";
+            btnRelevador.Size = new Size(201, 64);
+            btnRelevador.TabIndex = 0;
+            btnRelevador.Text = "Relevador";
+            btnRelevador.UseVisualStyleBackColor = false;
+            btnRelevador.Click += btnRelevador_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(428, 165);
+            Controls.Add(btnRelevador);
+            Name = "Form1";
+            Text = "Control Relevador Arduino";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnRelevador;
+    }
+}
---
+++ ReleArduino/Form1.cs
@@ -0,0 +1,37 @@
+using System.IO.Ports;
+namespace ReleArduino
+{
+    public partial class Form1 : Form
+    {
+        bool bandera = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRelevador_Click(object sender, EventArgs e)
+        {
+            SerialPort puerto = new SerialPort();
+            puerto.PortName = "COM3";
+            puerto.BaudRate = 9600;
+            try {
+                puerto.Open();
+                if (bandera == false)
+                {
+                    puerto.WriteLine("e");
+                    btnRelevador.BackColor = Color.Lime;
+                }
+                else
+                {
+                    puerto.WriteLine("a");
+                    btnRelevador.BackColor = Color.IndianRed;
+                }
+                puerto.Close();                
+            }
+            catch (Exception ex) {
+                MessageBox.Show("Error" + ex.ToString()); }
+
+            
+        }
+    }
+}
